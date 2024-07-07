b83d47ad508cde9c50f81d0b1f0e87c17dd8cec9
taku <takumi.f.0308@icloud.com>
2024-07-07 22:58:44 +0900

    ハンク実験　コミット1

--- first.go
+++ first.go
@@ -1,6 +1,14 @@
 package main
 import "fmt"
 
+func add(a, b int) int {
+	return a + b
+}
+
+func diff (a, b int) int {
+	return a - b
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("Second, World!")
