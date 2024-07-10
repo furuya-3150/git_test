41e2a5ada87ed4395216d163d8c76e0306ab8e2e
taku <takumi.f.0308@icloud.com>
2024-07-10 23:23:11 +0900

    add secondAdd2_func to seconde.go

--- second.go
+++ second.go
@@ -4,6 +4,10 @@ func secondAdd(a, b int)int {
 	return a + b
 }
 
+func secondAdd2(a, b int)int {
+	return a + b + 2
+}
+
 func main() {
 	secondAdd(1, 2)
 	secondAdd(1, 2)
@@ -11,4 +15,6 @@ func main() {
 	secondAdd(1, 2)
 	secondAdd(1, 2)
 	secondAdd(1, 2)
+
+	secondAdd2(1, 2)
 }
